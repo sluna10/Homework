43b8cc2051a4aef717a08b3efb06d27ae228d62f
sluna10 <sluna@icstars.org>
2022-07-25 13:28:07 -0500

    Updated Homework Loop js

--- LoopsAssignment.js
+++ LoopsAssignment.js
@@ -18,35 +18,34 @@
 
 //NOTE Challenge: Instead of expecting an array of integers that represents the weight of the luggage, what if it's a string of comma separated integers?
 
-// function simulation () {
-//     items = Array.from(arguments)
-//     for(let a = 0; a <= items.length, a++ ) {
-//         // let luggages = []
-//         if(items[a] + items [a++] <= 40) {
-//             luggages = new Array(items[a], items [a++])
-//             console.log(luggages)
-//         }
-//         else {
-//             continue;
-//         }
-//     }
-// };
+function simulation () {
+    let items = Array.from(arguments)
+    let luggages = [[,],[]]
+    for(let a = 0; a <= items.length; a++ ) {
+        if(items[a] + items [a+ 1] <= 40) {
+            luggages = new Array(items[a], items [a++])
+            console.log(luggages)
+        }
+        else {
+            continue;
+        }
+    }
+};
 
 // simulation(30, 40, 10, 5, 5, 10, 10);
 
-const items = [30, 5, 40];
+// var items = [30, 5, 40];
 
-for (let a = 0; a <= items.length; ) {
-  const luggages = [];
-  const maxWeight = 40;
-  const totalWeight = items[a] + items[a + 1];
-  if (totalWeight <= maxWeight) {
-    luggages = items.splice(0, 2);
-  }
-  console.log(luggages);
-}
+// for (let a = 0; a <= items.length; ) {
+//   var luggages = [];
+//   var maxWeight = 40;
+//   var totalWeight = items[a] + items[a + 1];
+//   if (totalWeight <= maxWeight) {
+//     luggages = items.splice(0, 2);
+//   }
+//   console.log(luggages);
+// }
 
-// const items =
 
 // const luggages = items.splice(0, 2);
 
